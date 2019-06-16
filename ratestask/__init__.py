b828eead7dcc5b0576aff444aca1894e2f670285
Felipe Guilherme <felipeguilhermefs@gmail.com>
2019-06-16 09:41:31 -0300

    Create an error handler for param exception
    It will be needed since we will take params directly from requests and
    use to interact with the database. Sanitizing will be a must.

--- ratestask/__init__.py
+++ ratestask/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from .api_exception import InvalidQueryParam
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +8,11 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, World!'
+
+    @app.errorhandler(InvalidQueryParam)
+    def handle_invalid_query_param(error):
+        response = jsonify({'message': error.message})
+        response.status_code = 400
+        return response
 
     return app
